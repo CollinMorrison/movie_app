2aac0262fa4debbfe153d344af16b1f30033ef87
Collin Morrison <morricollin@gmail.com>
2024-01-27 13:39:34 -0700

    Initial commit plus successful repsonse from TMDB

---
+++ webservice/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fetch = require('node-fetch');
+require('dotenv').config()
+
+const app = express();
+
+// Accepts a get request
+app.get('/movies', (req, res) => {
+    const titleToSearch = req.query.search;
+    console.log(titleToSearch);
+    // console.log('API key: ' + process.env.TMDB_API_KEY)
+
+    const url = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1';
+    const options = {method: 'GET', headers: {
+        accept: 'application/json',
+        authorization: process.env.TMDB_API_KEY}};
+
+    
+    fetch(url, options)
+        .then(res => res.json())
+        .then(json=> console.log(json))
+        .catch(err => console.error('error: ' + err));
+
+        const response = [];
+
+    res.send(`Successful response with search value: ${titleToSearch}`)
+});
+
+app.listen(3000, () => console.log('App is listening on port 3000.'));
