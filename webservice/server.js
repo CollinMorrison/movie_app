a44fd90a8f1d6593d9ec28ae4d35379a710e9a9f
Collin Morrison <morricollin@gmail.com>
2024-01-27 13:50:01 -0700

    Successfully getting info from IMDB

--- webservice/server.js
+++ webservice/server.js
@@ -10,20 +10,21 @@ app.get('/movies', (req, res) => {
     console.log(titleToSearch);
     // console.log('API key: ' + process.env.TMDB_API_KEY)
 
-    const url = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1';
+    const url = 'https://api.themoviedb.org/3/search/movie?query=' + titleToSearch + '&include_adult=false&language=en-US&page=1';
     const options = {method: 'GET', headers: {
         accept: 'application/json',
         authorization: process.env.TMDB_API_KEY}};
 
-    
+    let response
     fetch(url, options)
         .then(res => res.json())
-        .then(json=> console.log(json))
+        .then(json=> {console.log(json)
+                res.send(json)})
         .catch(err => console.error('error: ' + err));
 
-        const response = [];
+        // const response = [];
 
-    res.send(`Successful response with search value: ${titleToSearch}`)
+    // res.send(`Successful response with search value: ${titleToSearch} ${response}`)
 });
 
 app.listen(3000, () => console.log('App is listening on port 3000.'));
