892b87a71b0d4e4d0b8b929d960a2bc4115009cc
Collin Morrison <morricollin@gmail.com>
2024-02-06 20:23:49 -0700

    Adding simple documentation

--- webservice/test/test.js
+++ webservice/test/test.js
@@ -7,6 +7,9 @@ describe('hooks', function () {
     })
 })
 
+/**
+ * Health check - makes sure the API call is going through at all
+ */
 describe('TMDB API get request', function () {
     it('should return status 200 when placing a get request with a title to search', async function () {
         const movieTitle = 'The Lion King'
@@ -15,6 +18,9 @@ describe('TMDB API get request', function () {
   });
 });
 
+/**
+ * Tests the length of the data in the response, limit results to 10
+ */
 describe('API Response Length', function () {
     it('should return a response with length 10', async function () {
         const movieTitle = 'the Lion King'
